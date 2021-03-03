73044ae86e794c62a64e97c3f2a37f9f52844288
Ivan31128 <79948191+Ivan31128@users.noreply.github.com>
2021-03-02 18:32:08 -0600

    Mi hola mundo en js

---
+++ programa01.js
@@ -0,0 +1,14 @@
+< ! DOCTYPE  html >
+ < html >
+  < cuerpo >
+    < button  onclick  = "function1 ()" > preciona < / button >
+      < p  id =  "element1" > Bienbenido < / p >
+
+    < guión >
+        function function1 () {
+          documento . getElemtByID ( "elemento1" ) . innerHTML  = "Hola mundo" ;
+      }
+
+    < / script >
+  < / cuerpo >
+ < / html >
